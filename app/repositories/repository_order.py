0693d34276fb4382ed5c8b84e7a3fc4d7daeb2bc
bohdan0089 <bodiks18@gmail.com>
2026-05-07 13:37:52 +0200

    feat: add repositories for Tenant and Order

---
+++ app/repositories/repository_order.py
@@ -0,0 +1,51 @@
+from sqlalchemy.ext.asyncio import AsyncSession
+from app.schemas.schemas_order import CreateOrder, ResponseOrder, UpdateOrder
+from app.models.model_order import Order
+from sqlalchemy import select
+from app.core.enums import OrderStatus
+
+
+class RepositoryOrder:
+    def __init__(self, session: AsyncSession):
+        self.session = session
+
+    async def create_order(self, data: CreateOrder) -> Order:
+        order = Order(
+            **data.model_dump()
+        )
+        self.session.add(order)
+        await self.session.flush()
+        await self.session.refresh(order)
+        return order
+
+    async def get_order(self, order_id: int) -> Order:
+        order = await self.session.get(Order, order_id)
+        return order
+    
+    async def get_orders(self, limit: int, offset: int) -> list[Order]:
+        orders = await self.session.execute(
+            select(Order)
+            .limit(limit).offset(offset)
+        )
+        return orders.scalars().all()
+    
+    async def orders_update(self, order: Order, data: UpdateOrder) -> Order:
+        for field, value in data.model_dump(exclude_unset=True).items():
+            setattr(order, field, value)
+        self.session.add(order)
+        await self.session.flush()
+        await self.session.refresh(order)
+        return order
+    
+    async def update_order_status(self, order: Order, status: OrderStatus) -> Order:
+        order.status = status
+        self.session.add(order)
+        await self.session.flush()
+        await self.session.refresh(order)
+        return order
+    
+    async def order_delete(self, order: Order):
+        await self.session.delete(order)
+        
+
+
---
+++ app/repositories/repository_tenant.py
@@ -0,0 +1,40 @@
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import select
+from app.schemas.schemas_tenant import CreateTenant, UpdateTenant
+from app.models.model_tenant import Tenant
+
+
+class RepositoryTenant:
+    def __init__(self, session: AsyncSession):
+        self.session = session
+
+    async def create_tenant(self, data: CreateTenant) -> Tenant:
+        tenant = Tenant(
+            **data.model_dump()
+        )
+        self.session.add(tenant)
+        await self.session.flush()
+        await self.session.refresh(tenant)
+        return tenant
+    
+    async def get_tenant(self, tenant_id: int) -> Tenant:
+        tenant = await self.session.get(Tenant, tenant_id)
+        return tenant
+    
+    async def get_tenants(self, limit: int, offset: int) -> list[Tenant]:
+        tenants = await self.session.execute(
+            select(Tenant)
+            .limit(limit).offset(offset)
+        )
+        return tenants.scalars().all()
+    
+    async def tenant_update(self, tenant: Tenant, data: UpdateTenant) -> Tenant:
+        for field, value in data.model_dump(exclude_unset=True).items():
+            setattr(tenant, field, value)
+        self.session.add(tenant)
+        await self.session.flush()
+        await self.session.refresh(tenant)
+        return tenant
+    
+    async def delete_tenant(self, tenant: Tenant):
+        await self.session.delete(tenant)
